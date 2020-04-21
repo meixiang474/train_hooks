f4fcc926a35226cb764c85d7b4abe003627f543d
meixiang474 <1436098850@qq.com>
2020-04-21 16:16:01 +0800

    学习hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useContext } from 'react';
 
-function App() {
+function App (props) {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        {count}
+      </div>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </>
+  )
 }
-
-export default App;
